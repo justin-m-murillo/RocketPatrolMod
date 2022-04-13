1d8b4cf2f8e100b85550ecab19d9d1027cb8d9d2
justin-m-murillo <murillo_justin@yahoo.com>
2022-04-13 14:57:03 -0700

    Retracted Changes to create Game Over Menu

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
-    scene: [ Menu, Play]
+    scene: [ Menu, Play, GameOver]
 }
 
 let game = new Phaser.Game(config);
 
 // reserve keyboard vars
-let keyF, keyR, keyLEFT, keyRIGHT;
+let keyF, keyR, keyM, keyLEFT, keyRIGHT;
 
 // set UI sizes
 let borderUISize = game.config.height / 15;
