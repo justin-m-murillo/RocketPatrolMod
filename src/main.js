8ddebf62b4d4b5f5bef16006d9f98f3b69661a39
Justin Murillo <murillo_justin@yahoo.com>
2022-04-15 13:50:48 -0700

    Program description added to main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,38 @@
+/*
+
+Justin Murillo
+Rocket Patrol Mod
+04/15/2022
+Completion Time: > 10 hours
+
+ROCKET PATROL MODIFICATIONS
+
+1.) Updated weapon asset and mechanics (20 pts)
+    a.) Weapon is now a cannon that shoots a rocket
+    b.) Revised rocket asset to be circular
+    c.) Implemented a charging mechanic for the rocket. Longer charges increase the speed of the projectile. 
+2.) For single player, implemented mouse controls and removed keyboard controls for the cannon only. (20 pts)
+3.) Implemented add-time bonus (1 sec.) for every successful hit on enemy spaceships. (20 pts)
+4.) Added a countdown timer display on the top-right corner of the Play scene. Displays time remaining in game. (10 pts)
+5.) Implemented simultaneous two-player mode (30 pts)
+
+POINTS
+
+    1 (20)
+    2 (20)
+    3 (20)
+    4 (10)
+    5 (30)
+    ------
+Total 100
+
+
+WORKS CITED
+
+https://photonstorm.github.io/phaser3-docs/ - Referenced document for all code
+
+*/
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
